c5b02f56b4dea5e323bd173b932956e1f92d6732
Kaixu Yang <kaixuyang@gmail.com>
2021-07-28 17:28:58 -0700

    Update docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ import datetime
 import os
 import pathlib
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join("..")))
+sys.path.insert(0, os.path.abspath(os.path.join("..")))  # adds path to find greykite folder during doc build.
 
 from sphinx_gallery.sorting import FileNameSortKey
 from greykite.common.sphinx_plotly import plotly_sg_scraper
