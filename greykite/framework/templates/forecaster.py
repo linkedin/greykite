0ab2af785b7772ac2ffeebc4a2744be7a584e00d
McKenzie Quinn <mckenzie.quinn@allegion.com>
2021-08-26 09:35:43 -0400

    add code to be make dumps/loads compatiable with Google Cloud Storage

--- greykite/framework/templates/forecaster.py
+++ greykite/framework/templates/forecaster.py
@@ -45,7 +45,8 @@ from greykite.framework.templates.pickle_utils import load_obj
 from greykite.framework.templates.simple_silverkite_template import SimpleSilverkiteTemplate
 from greykite.framework.templates.template_interface import TemplateInterface
 from greykite.sklearn.estimator.one_by_one_estimator import OneByOneEstimator
-
+from greykite.framework.templates.gcp_utils import dump_obj_cloud
+from greykite.framework.templates.gcp_utils import load_obj_cloud
 
 class Forecaster:
     """The main entry point to creates a forecast.
@@ -362,6 +363,7 @@ class Forecaster:
     def dump_forecast_result(
             self,
             destination_dir,
+            bucket_name=None,
             object_name="object",
             dump_design_info=True,
             overwrite_exist_dir=False):
@@ -387,17 +389,29 @@ class Forecaster:
         """
         if self.forecast_result is None:
             raise ValueError("self.forecast_result is None, nothing to dump.")
-        dump_obj(
-            obj=self.forecast_result,
-            dir_name=destination_dir,
-            obj_name=object_name,
-            dump_design_info=dump_design_info,
-            overwrite_exist_dir=overwrite_exist_dir
-        )
+        if not bucket_name:
+            dump_obj(
+                obj=self.forecast_result,
+                dir_name=destination_dir,
+                obj_name=object_name,
+                dump_design_info=dump_design_info,
+                overwrite_exist_dir=overwrite_exist_dir
+            )
+        else:
+            dump_obj_cloud(
+                obj=self.forecast_result,
+                dir_name=destination_dir,
+                bucket_name = bucket_name,
+                obj_name=object_name,
+                dump_design_info=dump_design_info,
+                overwrite_exist_dir=overwrite_exist_dir)
+
+
 
     def load_forecast_result(
             self,
             source_dir,
+            bucket_name=None,
             load_design_info=True):
         """Loads ``self.forecast_result`` from local files created by ``self.dump_result``.
 
@@ -412,8 +426,16 @@ class Forecaster:
         """
         if self.forecast_result is not None:
             raise ValueError("self.forecast_result is not None, please create a new instance.")
-        self.forecast_result = load_obj(
-            dir_name=source_dir,
-            obj=None,
-            load_design_info=load_design_info
-        )
+        if not bukcet_name:
+            self.forecast_result = load_obj(
+                dir_name=source_dir,
+                obj=None,
+                load_design_info=load_design_info
+            )
+        else:
+            self.forecast_result = load_obj_cloud(
+                dir_name=source_dir,
+                bucket_name = bucket_name,
+                obj=None,
+                load_design_info=load_design_info
+            )
