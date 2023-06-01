68fbec2c13b66cb24583c85c825cff676aa02c38
Kathy(Chenying)Gao <cg2908@columbia.edu>
2023-06-01 13:52:04 -0400

    Update setup.py to 0.5.1

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/linkedin/greykite",
-    version="0.5.0",
+    version="0.5.1",
     zip_safe=False,
 )
